b47c79622900a8b96a21111a9228d4909a06ebe3
2005leonorlin <leonor.s353942@cesurformacion.com>
2025-06-06 12:44:39 +0200

    Añadimos component de monster

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { MonsterComponent } from './monster/monster';
+export const routes: Routes = [
+    {path:'' , component:MonsterComponent}
+];
---
+++ src/app/monster/monster.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-monster',
+  imports: [],
+  standalone:true,
+  templateUrl: './monster.html',
+  styleUrl: './monster.scss'
+})
+export class MonsterComponent {
+}
