5c730774ee0d060575cc4c6e882912718e46a03d
RocioProjects <rocoarchivos@gmail.com>
2025-06-06 12:52:29 +0200

    Revert "Se añade angular cli"
    This reverts commit 5987652ef74131c30e07d169b1ad11539bd522c7.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import {SpellsComponent} from './spells/spells';
-export const routes: Routes = [
-    {
- path:'',
- component: SpellsComponent,
 
-}
-];
+export const routes: Routes = [];
