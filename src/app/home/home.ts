3a65295eb863cf20c48b3e15cde8c9dd5e1e3ac3
Hi-Virby <hanerxd@outlook.com>
2025-06-09 13:14:32 +0200

    Route update fix

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
+  imports: [RouterLink],
   standalone: true,
   templateUrl: './home.html',
   styleUrls: ['./home.scss']
