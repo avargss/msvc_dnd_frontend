7d44731c6b9cb85d29a59b116e6472a9f9d52f18
RocioProjects <rocoarchivos@gmail.com>
2025-06-09 12:49:55 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home';
+//import { SpellsComponent } from './spells/spells';
+//import { MonsterComponent } from './monster/monster';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
     },
+   /* {
+        path: 'monster',
+        loadComponent: () => import('./monster/monster').then(m => m.monster),
+    },
+ 
+    {
+        path: 'spells',
+        loadComponent: () => import('./spells/spells').then(m=> m.spells),
+ } */
+
     
 ];
