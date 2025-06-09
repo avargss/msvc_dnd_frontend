3e690a497510cbd9e48ba28dc144f8bcb3211614
2005leonorlin <leonor.s353942@cesurformacion.com>
2025-06-09 16:24:05 +0200

    api de monstruos

---
+++ src/app/monServices/monster.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MonsterService {
+  private apiUrl =`https://www.dnd5eapi.co/api/monsters`
+  constructor(private http: HttpClient) {}
+
+    getMonster():Observable<any>{
+      return this.http.get<any>(this.apiUrl);
+    }
+   
+}
