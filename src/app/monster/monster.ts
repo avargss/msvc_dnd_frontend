1327325e47d36d881d92262f69a8ec45d76d0434
Hi-Virby <hanerxd@outlook.com>
2025-06-10 09:38:02 +0200

    Monster data update

--- src/app/monster/monster.ts
+++ src/app/monster/monster.ts
@@ -1,6 +1,8 @@
 import { Component} from '@angular/core';
 import { MonsterService } from '../monServices/monster';
 import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { forkJoin } from 'rxjs';
+
 
 
 @Component({
@@ -16,14 +18,32 @@ export class MonsterComponent {
   mostrarTabla:boolean = false;
   constructor(private monsterService: MonsterService){}
 
-  accion(){
-    console.log(`Boton " ver monstruos" `);
-    this.monsterService.getMonster().subscribe( response =>{
-      this.monsters = response.results;
-      this.mostrarTabla =true;
+  accion() {
+  console.log(`Botón "Ver monstruos" presionado`);
+  this.monsters = [];
+  this.mostrarTabla = false;
+
+  this.monsterService.getMonster().subscribe(response => {
+    const allMonsters = response.results;
+    const randomMonsters = this.getRandomMonsters(allMonsters, 10);
+
+    const requests = randomMonsters.map(monster =>
+      this.monsterService.getMonsterDetails(monster.index)
+    );
+
+    forkJoin(requests).subscribe(monsterDetails => {
+      this.monsters = monsterDetails;
+      this.mostrarTabla = true;
     });
-    
-  }
+  });
+}
+
+
+getRandomMonsters(monsters: any[], count: number): any[] {
+  const shuffled = monsters.sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, count);
+}
+
 }
   
 
