40a10897f94cf4ba34d6fad7d41e60e1768a5d1b
2005leonorlin <leonor.s353942@cesurformacion.com>
2025-06-10 17:41:39 +0200

    Revert "Dnd 012"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,3 @@
+import { Routes } from '@angular/router';
 
-
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home';
-import { MonsterComponent } from './monster/monster';
-import { SpellsComponent } from './spells/spells';
-
-export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent,
-    },
-    {
-        path: 'monster',
-        component: MonsterComponent,
-    },
- 
-    {
-        path: 'spells',
-        component: SpellsComponent,
-    } 
-
-    
-];
+export const routes: Routes = [];
